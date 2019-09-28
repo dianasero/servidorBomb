eeeeaf3e53237d6ccd15fa726af7b81b59330a47
dianasero <dianalehm.15@gmail.com>
2019-09-28 15:50:38 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express();
+let controlMaq1 = false, controlMaq2 = true;
+app.get('/', (req, res) => {
+  res.send('Hello Diana ')
+});
+app.get('/apagado', (req, res) => {
+	controlMaq1 = false;
+  res.send('0')
+});
+app.get('/apagado2', (req, res) => {
+	controlMaq2 = false;
+  res.send('0')
+});
+app.get('/encendido', (req, res) => {
+	controlMaq1 = true;
+  res.send('1')
+});
+app.get('/encendido2', (req, res) => {
+	controlMaq2 = true;
+  res.send('1')
+});
+app.get('/consulta', (req, res) => {
+  res.send(controlMaq1+" "+controlMaq2)
+});
+app.get('/RutaDats/:datos', (req, res) => {//Datos variable que se leee
+  res.send('Datos leido: '+ req.params.datos)
+});
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
